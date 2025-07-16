2b78d523452ab682cc23541be471111cdbb0805e
vinhpbui <vinhphucvn@yahoo.com>
2025-07-16 01:26:45 -0700

    latest

--- puppy.go
+++ puppy.go
@@ -18,3 +18,15 @@ func BigBark() string {
 func BigBarks() string {
     return dog.WhenGrownUp(Barks())
 }
+
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
+}
+
+func From12() {
+	fmt.Println("I'm from version 1.2.0")
+}
+
+func From13() {
+	fmt.Println("I'm from version 1.3.0")
+}
