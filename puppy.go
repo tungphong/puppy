5395cd7de027a5828245ba65c746452625b8a515
vinhpbui <vinhphucvn@yahoo.com>
2025-07-16 17:34:42 -0700

    pbvci - version 1.1.0

--- puppy.go
+++ puppy.go
@@ -23,10 +23,3 @@ func From11() {
 	fmt.Println("I'm from version 1.1.0")
 }
 
-func From12() {
-	fmt.Println("I'm from version 1.2.0")
-}
-
-func From13() {
-	fmt.Println("I'm from version 1.3.0")
-}
